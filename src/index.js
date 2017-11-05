beef51970f26f98f93eed9b1a445f6b2871233e5
acgfreshman <liumuchen1996@gmail.com>
2017-11-05 11:43:27 -0800

    finish app component with videos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import SearchBar from './components/search_bar'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 const API_KEY = 'AIzaSyDosjh0rHteAkGwRqqZx6kpDAQoUpkiAtY';
 
 
 //Create a new component
 //produce some HTML
-const App = () => {
-    return(
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos:[],
+            selectedVideo: null
+        } ;
+
+        YTSearch({key:API_KEY, term:'surfboards'}, (videos) =>{
+            this.setState({
+                videos:videos,
+                selectedVideo:videos[0]
+                }
+            );
+            //this.setState({videos:videos}); if argument and property
+            // is the same
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo } />
+                <VideoList
+                    onVideoSelect = { selectedVideo =>
+                        this.setState({selectedVideo})
+                    }
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 

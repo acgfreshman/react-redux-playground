c07272eb14f65185bbee2056b9114c5d176a4d33
acgfreshman <liumuchen1996@gmail.com>
2017-12-21 17:19:47 -0800

    finish weather component

--- redux advance example/ReduxSimpleStarter-master/src/containers/weather_list.js
+++ redux advance example/ReduxSimpleStarter-master/src/containers/weather_list.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from 'react-sparklines'
+import Chart from '../components/chart';
 
 class WeatherList extends React.Component{
     renderWeather(cityData){
         //pull off data
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
-
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{ name }</td>
-                <td>
-                    <Sparklines height={120} width={180} data={temps}>
-                        <SparklinesLine color="red" />
-                    </Sparklines>
-                </td>
+                <td><Chart data={temps} color="orange" unit="K"/></td>
+                <td><Chart data={pressures} color="green" unit="hPa"/></td>
+                <td><Chart data={humidities} color="black" unit="%"/></td>
             </tr>
         );
-
-
 
     }
 
@@ -29,9 +27,9 @@ class WeatherList extends React.Component{
             <thead className="table table-hover">
             <tr>
                 <th>City</th>
-                <th>Temperature</th>
-                <th>Pressure</th>
-                <th>Humidity</th>
+                <th>Temperature (K)</th>
+                <th>Pressure (hPa)</th>
+                <th>Humidity %</th>
             </tr>
             </thead>
             <tbody>
